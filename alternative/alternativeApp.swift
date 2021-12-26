76428c9bb3744b9e610557fbe91584b4f920fb86
binay <binay.pg@gmail.com>
2021-12-26 21:24:21 +0530

    Initial Commit

---
+++ alternative/alternativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  alternativeApp.swift
+//  alternative
+//
+//  Created by Binay Kumar Sharma on 26/12/21.
+//
+
+import SwiftUI
+
+@main
+struct alternativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
